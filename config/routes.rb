09d212b902cf7ec26e851e0253572696f0a1d8b3
theotteryears <theotteryears@me.com>
2025-06-25 12:11:33 +0100

    change deploy.yml

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles do
+    resources :comments
+  end
 end
